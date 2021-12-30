dd53a92c3057153eb2a3e3e5b17d4f1da412e368
Frederico Silva <fred.webfront@gmail.com>
2021-12-30 19:34:31 -0300

    chore: base structure and configuration

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Tag = styled.header``;
+
+export const Logo = styled.svg``;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,5 @@
+import Header from '../components/Header';
+
+export function Index(): JSX.Element {
+  return <Header />;
+}
